6eecad1ffd084d28a274a89c641cd2a7abbfb79f
Elie Zananiri <ez@silentlycrashing.net>
2010-02-28 09:11:53 -0500

    Switched to document-based application.

---
+++ ScrapeDocument.h
@@ -0,0 +1,16 @@
+//
+//  ScrapeDocument.h
+//  Scrape
+//
+//  Created by Elie Zananiri on 10-02-28.
+//  Copyright 2010 silentlyCrashing::net. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface ScrapeDocument : NSDocument {
+    
+}
+
+@end
