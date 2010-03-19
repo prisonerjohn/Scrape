8def445054fab93c032a801c7e833f06b9dfe68a
Elie Zananiri <ez@silentlycrashing.net>
2010-03-19 12:11:04 -0400

    Consolidated ScrapeAppController and ScrapeAppDelegate since it's basically the same thing.

--- ScrapeAppDelegate.h
+++ ScrapeAppDelegate.h
@@ -10,12 +10,36 @@
 #import <Growl/Growl.h>
 
 
+extern NSString *ScrapeAutomaticSettingsChanged;
+extern NSString *ScrapeAutomaticToggleKey;
+extern NSString *ScrapeAutomaticMinKey;
+extern NSString *ScrapeAutomaticMaxKey;
+
+extern NSString *SiteRoot;
+
+@class ScrapePrefsController;
+
+
 #if (MAC_OS_X_VERSION_MAX_ALLOWED <= MAC_OS_X_VERSION_10_5)
 @interface ScrapeAppDelegate : NSObject <GrowlApplicationBridgeDelegate> {
 #else
 @interface ScrapeAppDelegate : NSObject <GrowlApplicationBridgeDelegate, NSApplicationDelegate> {
 #endif
-
+    IBOutlet NSMenu *statusMenu;
+    
+    NSStatusItem    *statusItem;
+    NSImage         *idleImage;
+    NSImage         *selectImage;
+    
+    ScrapePrefsController   *prefsController;
 }
+    
+- (void)scheduleAutomaticScrape;
+- (void)updateTimer:(NSNotification *)notification;
+- (void)newAutoScrape;
+
+- (IBAction)newManualScrape:(id)sender;
+- (IBAction)showAboutWindow:(id)sender;
+- (IBAction)showPrefsWindow:(id)sender;
 
 @end
